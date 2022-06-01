7b6393da4fa9da562e02570fc5a266ddc8d76f5a
optikrus <optikrus61@gmail.com>
2022-06-01 16:59:57 +0300

    edit admin

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,6 +1,24 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.admin.widgets import FilteredSelectMultiple
 
 from mainapp.models import QuizCategory, Quiz, Question, Answer
+
+
+class QuizAdminForm(forms.ModelForm):
+    class Meta:
+        model = Quiz
+        exclude = []
+
+    questions = forms.ModelMultipleChoiceField(
+        queryset=Question.objects.all().select_subclasses(), required=False,
+        label="вопросы", widget=FilteredSelectMultiple(verbose_name="вопросы", is_stacked=False))
+
+
+class AnswerAdminForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        exclude = []
 
 
 @admin.register(QuizCategory)
@@ -16,6 +34,7 @@ class QuestionAdminInline(admin.StackedInline):
 class AnswerAdminInline(admin.TabularInline):
     model = Answer
     extra = 0
+    form = AnswerAdminForm
 
 
 @admin.register(Question)
@@ -27,9 +46,9 @@ class QuestionAdmin(admin.ModelAdmin):
 
 @admin.register(Quiz)
 class QuizAdmin(admin.ModelAdmin):
+    form = QuizAdminForm
 
     list_display = ('title', 'category')
     list_filter = ('category',)
     inlines = (QuestionAdminInline, )
-
     search_fields = ('title', )
